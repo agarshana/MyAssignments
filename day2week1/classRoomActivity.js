647555a2d9da6ac33d10e3089552b0db36485f29
agarshana <58935762+agarshana@users.noreply.github.com>
2026-09-02 16:00:21 +0530

    Add files via upload

---
+++ day2week1/classRoomActivity.js
@@ -0,0 +1,19 @@
+var firstName = "Agarshana"
+console.log(firstName);
+console.log(typeof firstName);
+
+var companyName= "Google"
+console.log(companyName);
+console.log(typeof companyName);
+
+var mobileNumber= 5678532689
+console.log(mobileNumber);
+console.log(typeof mobileNumber);
+
+var isAutomation= true
+console.log(isAutomation);
+console.log(typeof isAutomation);
+
+var hasPlaywright
+console.log(hasPlaywright);
+console.log(typeof hasPlaywright);
