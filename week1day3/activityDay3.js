d9e6939df3da1bdae9d730aab7639e7a9c2006f4
agarshana <58935762+agarshana@users.noreply.github.com>
2026-09-03 13:59:19 +0530

    Add files via upload

---
+++ week1day3/activityDay3.js
@@ -0,0 +1,27 @@
+/*using var
+
+const browserVersion = "Chrome";
+
+function getBrowserVersion() {
+    if (browserVersion == "Chrome") {
+        var browserVersion = "Chrome";
+    }
+
+    console.log("browserVersion:" +browserVersion);
+}
+
+getBrowserVersion(); */
+
+//using let
+
+const browserVersion = "Chrome";
+
+function getBrowserVersion() {
+    if (browserVersion == "Chrome") {
+        let browserVersion = "Chrome";
+    }
+
+    console.log("browserVersion:" +browserVersion);
+}
+
+getBrowserVersion(); 
